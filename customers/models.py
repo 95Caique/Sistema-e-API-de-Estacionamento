a7c96804fc2a2c037de75f25a761da3d253b5a85
Caque <decaiquesouza@gmail.com>
2025-07-21 11:04:58 -0300

    Feature: Tabela de clientes

---
+++ customers/models.py
@@ -0,0 +1,22 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Customer(models.model):
+    user = models.OneToOneField(User,on_delete=models.Protect,blank=True,null=True,
+        related_name='customers',
+        verbose_nam='Usuário'
+    )
+    name = models.CharField(max_length=100, verbose_name='Nome')
+    cpf = models.CharField(max_length=11, blank=True, null= True, verbose_name= 'CPF')
+    phone = models.CharField(max_length=15, blank=True, null = True, verbose_name = 'Telefone')
+    created_at = models.DatetimeField(auto_now_add=True, verbose_name='Criado em')
+    updated_at = models.DatetimeField(auto_now=True, verbose_name='Atualizado em')
+
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+
+    def __str__(self):
+        return self.name
+
