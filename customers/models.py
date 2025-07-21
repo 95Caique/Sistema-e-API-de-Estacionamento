c16d4b755ba34a1ffd63249b5649bd4e2168fcb2
Caque <decaiquesouza@gmail.com>
2025-07-21 11:52:07 -0300

    Feature: App e models de veículos feita estrutura inicial

--- customers/models.py
+++ customers/models.py
@@ -2,16 +2,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Customer(models.model):
-    user = models.OneToOneField(User,on_delete=models.Protect,blank=True,null=True,
+class Customer(models.Model):
+    user = models.OneToOneField(User,on_delete=models.PROTECT,blank=True,null=True,
         related_name='customers',
-        verbose_nam='Usuário'
+        verbose_name='Usuário'
     )
-    name = models.CharField(max_length=100, verbose_name='Nome')
-    cpf = models.CharField(max_length=11, blank=True, null= True, verbose_name= 'CPF')
-    phone = models.CharField(max_length=15, blank=True, null = True, verbose_name = 'Telefone')
-    created_at = models.DatetimeField(auto_now_add=True, verbose_name='Criado em')
-    updated_at = models.DatetimeField(auto_now=True, verbose_name='Atualizado em')
+    name = models.CharField(max_length=100, verbose_name='Nome',)
+    cpf = models.CharField(max_length=11, blank=True, null= True, verbose_name= 'CPF',)
+    phone = models.CharField(max_length=15, blank=True, null = True, verbose_name = 'Telefone',)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Criado em',)
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Atualizado em',)
 
     class Meta:
         verbose_name = 'Cliente'
