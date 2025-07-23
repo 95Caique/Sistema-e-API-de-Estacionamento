0a2c460bdc5fa554393a10337ab626771e3a37c2
Caque <decaiquesouza@gmail.com>
2025-07-23 09:44:17 -0300

    Feature: Classes registradas no admin, todas já funcionando e registrando corretamente

--- parking/admin.py
+++ parking/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from parking.models import ParkinSpot, ParkingRecord
+
+
+@admin.register(ParkinSpot)
+class ParkinSpotAdmin(admin.ModelAdmin):
+    list_display = ['spot_number', 'is_occupied']
+    search_fields = ['spot_number']
+
+
+@admin.register(ParkingRecord)
+class ParkingRecordAdmin(admin.ModelAdmin):
+    list_display = ['vehicle', 'parking_spot', 'entry_time', 'exit_type']
+    search_fields = ['vehicle__licence_plate', 'parking_spot__spot_number']
+
