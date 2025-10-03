4adfe68404b38bd5fac70a2d60cc6ce304a296d4
Caque <decaiquesouza@gmail.com>
2025-10-03 19:57:48 -0300

    Feature: Implementação de djangopermissions para controlar as permissões de visualização das telas e acessos da api

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAdminUser, DjangoModelPermissions
 from rest_framework import viewsets
 
 
@@ -7,3 +8,5 @@ from customers.serializers import CustomerSerializer
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [DjangoModelPermissions, IsAdminUser]
+
